67f69a8ce1a4034542ddabb29d881bfbd9557c63
Isuru657 <Agampodi.I.Abeysekara.22@dartmouth.edu>
2021-09-28 19:46:41 -0400

    Training data created here

--- train_model.py
+++ train_model.py
@@ -50,3 +50,47 @@ pickle.dump(words, open('words.pkl','wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
 print("Finished preprocessing")
+
+# create training data
+# the input for the model is a pattern and the output is the 
+# class it belongs to.
+
+training_data = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(w.lower()) for w in pattern_words]
+
+    # loops through corpus of words, if that word exists in the pattern words, it gets 1 else 0
+    for w in words:
+        if w in pattern_words:
+            bag.append(1)
+        else:
+            bag.append(0)
+    
+
+    # 0 for all classes and 1 for current class. Class = tag
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training_data.append([bag, output_row])
+
+import random
+import numpy as np
+
+# Suppressing the warning: train_model.py:84: VisibleDeprecationWarning: Creating an ndarray from ragged nested sequences (which is a list-or-tuple of lists-or-tuples-or ndarrays with different lengths or shapes) is deprecated. 
+# If you meant to do this, you must specify 'dtype=object' when creating the ndarray.
+
+np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
+
+random.shuffle(training_data)
+training = np.array(training_data)
+
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+
+
+print("Training data created")
